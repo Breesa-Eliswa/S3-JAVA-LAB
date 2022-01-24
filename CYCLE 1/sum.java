c420b55b7d6b4ee7ce5c28f0081ffb7be6e30bbb
Breesa Eliswa Manoj <85623286+Breesa123@users.noreply.github.com>
2022-01-24 07:30:57 +0530

    Create sum.java

---
+++ CYCLE 1/sum.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class sum
+{
+    public static void main(String agrs[])
+    {
+    Scanner num = new Scanner(System.in);
+    System.out.println("Enter the no:s to be added:");
+    int num1 = num.nextInt();
+    int num2 = num.nextInt();
+    int sum = num1 + num2 ;
+    System.out.println("Sum of the numbers is " +sum);
+    }
+}
