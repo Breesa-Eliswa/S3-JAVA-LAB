254668e0f249950729de284dc12ea5cc3bacc777
Breesa Eliswa Manoj <85623286+Breesa123@users.noreply.github.com>
2022-02-14 21:56:40 +0530

    Create file4.java

---
+++ CYCLE 5/file4.java
@@ -0,0 +1,32 @@
+import java.io.*;
+import java.util.*;
+class file4
+{
+    public static void main(String[] args) throws Exception
+    {
+	try
+	{
+    FileInputStream fin = new FileInputStream("e3.txt");
+    int ch;
+	  String a;
+    int sum=0;
+	  System.out.println("The Numbers in the file are : ");
+	  while( (ch = fin.read())!=-1 )  
+    {
+		String s=Character.toString((char)ch);
+		StringTokenizer st = new StringTokenizer(s);
+		while( st.hasMoreTokens()) 
+		{
+			a = st.nextToken();
+			int b = Integer.parseInt(a);
+			System.out.println(b);
+			sum = sum + b; 
+		}
+	}
+	
+	System.out.println("The sum is " + sum);
+	}
+	catch(Exception e) {System.out.print(e);}
+	}
+
+}
