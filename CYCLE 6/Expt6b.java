2f97e249e55718bb418a4bebb15fc09b2f077071
Breesa Eliswa Manoj <85623286+Breesa123@users.noreply.github.com>
2022-02-20 22:20:05 +0530

    Create Expt6b.java

---
+++ CYCLE 6/Expt6b.java
@@ -0,0 +1,16 @@
+class Expt6b
+{
+	public static void main(String[] args)
+	{
+		try
+		{
+			
+			int a[] = {1,2,3,4,5,6,7,8};
+			System.out.println(a[11]);
+		}
+		catch(ArrayIndexOutOfBoundsException e)
+		{
+			System.out.println(e);
+		}
+	}
+}
